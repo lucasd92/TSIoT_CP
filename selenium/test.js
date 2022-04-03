f40423a9ef46d4e0336df494bf1a4c79b44715c6
Lucas <lucasd92@hotmail.com>
2022-04-03 20:23:17 +0000

    Added multiply endpoint + tests

--- selenium/test.js
+++ selenium/test.js
@@ -2,8 +2,10 @@ const {Builder, By, until, Key, Capabilities} = require('selenium-webdriver');
 const {expect} = require('chai');
 var firefox = require('selenium-webdriver/firefox');
 //var profilePath = '/home/tsiot/.mozilla/firefox/zoa6kvyg.default';
-var profilePath = '/home/tsiot/.mozilla/firefox/8297bw4u.default-release';
-let TIMEOUT=10000;
+var profilePath = '/home/lucas/.mozilla/firefox/m4nw1bz0.default-release';
+
+//var profilePath = '/home/tsiot/.mozilla/firefox/8297bw4u.default-release';
+let TIMEOUT=20000;
 
 describe('test multi site with firefox', function() {
    let driver;
@@ -65,6 +67,16 @@ describe('test multi site with firefox', function() {
       driver.findElement(By.id('canary')).then(element=>{
          expect(element.text).to.equal('Canario');  
       });
+   });
+
+   it('check that endpoint multiply multiplies a by b', async function() {
+      this.timeout(12000);
+      await driver.get('https://sensor/multiply');
+      await driver.findElement(By.id('a')).then(element => element.sendKeys('6'));
+      await driver.findElement(By.id('b')).then(element => element.sendKeys('9'));
+      await driver.findElement(By.id('calculate')).then(element => element.click());
+      let value = await driver.findElement(By.id('result')).getText();
+      expect(value).to.equal('The result of multiplying 6 by 9 is 54');
    });
 
    after( () =>
